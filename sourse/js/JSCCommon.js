4a14a19a43dfd223a6db693e963c2deea5b0fd82
JulietBrn <shishkina.ja@gmail.com>
2024-01-16 08:11:52 +0100

    feat: add fancybox gallery

--- sourse/js/JSCCommon.js
+++ sourse/js/JSCCommon.js
@@ -395,7 +395,7 @@ class JSCCommon {
 
   static init() {
     this.modalCall();
-    // this.tabscostume('tabs');
+    this.tabscostume('tabs');
     this.mobileMenu();
     this.inputMask();
     // this.sendForm();
